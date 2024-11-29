a40b47e58ce571d9b6b13e5845f75ccef7fdbb00
202010850 김준형 <jh010303@naver.com>
2024-11-29 21:02:33 +0900

    인터넷 소켓 서버
    인터넷 소켓 서버

--- server.c
+++ server.c
@@ -9,14 +9,14 @@
 
 #define PORTNUM 9005
 
-// UDP 동작 서버 
+// UDP 동시 동작 서버 
 int main(){
     char buf[BUFSIZ];
     struct sockaddr_in sin,cli;
-    int sd, clientlen = sizeof(cli);
+    int sd,ns,clientlen = sizeof(cli);
     
     // 인터넷 소켓, UDP 사용
-    if((sd=socket(AF_INET,SOCK_DGRAM,0))==-1){ // 서버측 udp 소켓
+    if((sd=socket(AF_INET,SOCK_STREAM,0))==-1){ // 서버측 udp 소켓
         perror("socket");
         exit(1);
     }
@@ -32,21 +32,60 @@ int main(){
         exit(1);
     }
 
-    // 반복 실행
+    if(listen(sd,3)){
+        perror("listen");
+        exit(1);
+    }
+    // 동시 동작 서버 실행
     while(1){
-        // cli에는 송신자의 주소 담겨있음. clen에는 주소 구조체 크기 
-        if((recvfrom(sd,buf,BUFSIZ,0,(struct sockaddr*)&cli, &clientlen))==-1){ // client가 server에게 쏜 문자 받음, buf에 저장
-            perror("recvfrom");
+        if((ns = accept(sd,(struct sockaddr*)&cli,&clientlen))==-1){
+            perror("accept");
             exit(1);
         }
 
-        printf("From Client : %s\n",buf);
-        strcpy(buf,"Hello Client.");
-    
-        // cli에는 수신자의 주소. 
-        if(sendto(sd,buf,strlen(buf)+1,0,(struct sockaddr*)&cli, sizeof(cli))==-1){ // client가 server에게 메세지 전송
-            perror("sendto");
-            exit(1);
+        switch(fork()){
+            case 0:
+                strcpy(buf,"Welcome to Server. What's your name?");
+                if(send(ns,buf,strlen(buf)+1,0)==-1){
+                    perror("send");
+                    exit(1);
+                }
+
+                if(recv(ns,buf,sizeof(buf),0)==-1){
+                    perror("recv");
+                    exit(1);
+                }
+
+                printf("From Client:%s\n",buf);
+                while(1){
+                    sleep(1);
+                    if(send(ns,buf,strlen(buf)+1,0)==-1){
+                        perror("send");
+                        exit(1);
+                    }
+                    if(recv(ns,buf,sizeof(buf),0)==-1){
+                        perror("recv");
+                        exit(1);
+                    }
+                    printf("Message : %s\n",buf);
+                }
+                close(ns);
+                sleep(1);
+                exit(0);
         }
+        // if((recvfrom(sd,buf,BUFSIZ,0,(struct sockaddr*)&cli, &clientlen))==-1){ // client가 server에게 쏜 문자 받음, buf에 저장
+        //     perror("recvfrom");
+        //     exit(1);
+        // }
+
+        // printf("From Client : %s\n",buf);
+        // strcpy(buf,"Hello Client.");
+    
+        // // cli에는 수신자의 주소. 
+        // if(sendto(sd,buf,strlen(buf)+1,0,(struct sockaddr*)&cli, sizeof(cli))==-1){ // client가 server에게 메세지 전송
+        //     perror("sendto");
+        //     exit(1);
+        // }
     }
+    close(sd);
 }
