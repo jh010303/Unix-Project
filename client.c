ba613090aa9d57a8f4c72a7c9b84e292cdbac94b
JUNE <jh010303@naver.com>
2024-12-04 00:59:28 +0900

    .
    .

--- client.c
+++ client.c
@@ -9,14 +9,19 @@
 
 #define PORTNUM 9005
 
-// UDP 동작 서버 
+// TCP client 
 int main(){
     char buf[BUFSIZ],name[20];
-    struct sockaddr_in sin;
-    int sd, n;
+    struct sockaddr_in sin,cli;
+    int sd, sd2, n,len = sizeof(sin);
     
-    // 인터넷 소켓, UDP 사용
-    if((sd=socket(AF_INET,SOCK_STREAM,0))==-1){ // 서버측 udp 소켓
+    FILE* fs;
+    fs = fopen("ip_address.txt", "r");
+    char str[BUFSIZ];
+    fgets(str,BUFSIZ,fs);
+
+    // 인터넷 소켓, TCP 사용
+    if((sd=socket(AF_INET,SOCK_STREAM,0))==-1){ // client 소캣
         perror("socket");
         exit(1);
     }
@@ -25,32 +30,59 @@ int main(){
 
     sin.sin_family = AF_INET; // 소켓 주소 구조체에 유닉스 도메인 소켓임을 알림
     sin.sin_port = htons(PORTNUM);
-    sin.sin_addr.s_addr = inet_addr("192.168.56.1");   
+    sin.sin_addr.s_addr = inet_addr(str);   
 
+    // 서버에 연결
     if(connect(sd,(struct sockaddr*)&sin,sizeof(sin))){
         perror("connect");
         exit(1);
     }
 
-    if(recv(sd,buf,sizeof(buf),0)==-1){
-        perror("recv");
-        exit(1);
-    }
+    printf("server connected\n");
+
     printf("%s\n",buf);
+
+    // 닉네임 입력
+    printf("nickname: ");
     scanf("%s",name);
     strcpy(buf,name);
+
+    // 서버에게 닉네임 전달
     if(send(sd,buf,sizeof(buf)+1,0)==-1){
         perror("send");
         exit(1);
     }
-    
-    while(1){
-        printf("Message : ");
-        scanf("%s",buf);
-        if(send(sd,buf,sizeof(buf)+1,0)==-1){
-            perror("send");
-            exit(1);
-        }
+
+    // 서버에게 다른 클라이언트 정보 받음
+    if(recv(sd,&cli,sizeof(cli),0)==-1){
+        perror("recv");
+        exit(1);
     }
+
+    printf("%d\n",ntohs(cli.sin_port));
+    printf("%s\n",inet_ntoa(cli.sin_addr));
+    
+    // 새로운 소켓을 통해 다른 클라이언트에 연결    
+    if((sd2 = socket(AF_INET,SOCK_STREAM,0))==-1){
+        perror("socket");
+        close(sd);
+        exit(1);
+    }
+
+    if(connect(sd2,(struct sockaddr*)&cli,sizeof(cli))==-1){
+        perror("connect");
+        close(sd);
+        close(sd2);
+        exit(1);
+    }
+
+    while(1){
+        scanf("%s",buf);
+        send(sd2,buf,strlen(buf)+1,0);
+        recv(sd2,buf,BUFSIZ,0);
+        printf("Message: %s\n",buf);
+    }
+
     close(sd);
+    close(sd2);
 }
--- server.c
+++ server.c
@@ -9,14 +9,23 @@
 
 #define PORTNUM 9005
 
-// UDP 동시 동작 서버 
+struct user_info{
+    char username[20];
+};
+
+// TCP 서버 
 int main(){
     char buf[BUFSIZ];
-    struct sockaddr_in sin,cli;
-    int sd,ns,clientlen = sizeof(cli);
+    struct sockaddr_in sin,cli1,cli2;
+    int sd,cd1,cd2,clientlen = sizeof(cli1);
     
-    // 인터넷 소켓, UDP 사용
-    if((sd=socket(AF_INET,SOCK_STREAM,0))==-1){ // 서버측 udp 소켓
+    FILE* fs;
+    fs = fopen("ip_address.txt", "r");
+    char str[BUFSIZ];
+    fgets(str,BUFSIZ,fs);
+
+    // 서버 소켓
+    if((sd=socket(AF_INET,SOCK_STREAM,0))==-1){ // TCP, 인터넷 소켓
         perror("socket");
         exit(1);
     }
@@ -25,67 +34,58 @@ int main(){
 
     sin.sin_family = AF_INET; // 소켓 주소 구조체에 유닉스 도메인 소켓임을 알림
     sin.sin_port = htons(PORTNUM);
-    sin.sin_addr.s_addr = inet_addr("192.168.56.1");   
+    sin.sin_addr.s_addr = inet_addr(str);   
 
     if(bind(sd,(struct sockaddr*)&sin,sizeof(sin))){ // bind를 통해 소켓 기술자(sd)와 소켓 주소 구조체 연결
         perror("bind");
         exit(1);
     }
 
+    // 리스닝
     if(listen(sd,3)){
         perror("listen");
         exit(1);
     }
-    // 동시 동작 서버 실행
-    while(1){
-        if((ns = accept(sd,(struct sockaddr*)&cli,&clientlen))==-1){
-            perror("accept");
-            exit(1);
-        }
 
-        switch(fork()){
-            case 0:
-                strcpy(buf,"Welcome to Server. What's your name?");
-                if(send(ns,buf,strlen(buf)+1,0)==-1){
-                    perror("send");
-                    exit(1);
-                }
-
-                if(recv(ns,buf,sizeof(buf),0)==-1){
-                    perror("recv");
-                    exit(1);
-                }
-
-                printf("From Client:%s\n",buf);
-                while(1){
-                    sleep(1);
-                    if(send(ns,buf,strlen(buf)+1,0)==-1){
-                        perror("send");
-                        exit(1);
-                    }
-                    if(recv(ns,buf,sizeof(buf),0)==-1){
-                        perror("recv");
-                        exit(1);
-                    }
-                    printf("Message : %s\n",buf);
-                }
-                close(ns);
-                sleep(1);
-                exit(0);
-        }
-        // if((recvfrom(sd,buf,BUFSIZ,0,(struct sockaddr*)&cli, &clientlen))==-1){ // client가 server에게 쏜 문자 받음, buf에 저장
-        //     perror("recvfrom");
-        //     exit(1);
-        // }
-
-        // printf("From Client : %s\n",buf);
-        // strcpy(buf,"Hello Client.");
-    
-        // // cli에는 수신자의 주소. 
-        // if(sendto(sd,buf,strlen(buf)+1,0,(struct sockaddr*)&cli, sizeof(cli))==-1){ // client가 server에게 메세지 전송
-        //     perror("sendto");
-        //     exit(1);
-        // }
+    // client 1 연결됨
+    if((cd1 = accept(sd,(struct sockaddr*)&cli1, &clientlen))==-1){
+        perror("accept client1");
+        exit(1);
     }
+
+    printf("client1 connected\n");
+
+    // client1의 username 받음
+    if(recv(cd1,buf,sizeof(buf),0)==-1){
+        perror("recv");
+        exit(1);
+    }
+
+    printf("%s connected\n",buf);
+
+    // client 2 연결됨
+    if((cd2 = accept(sd,(struct sockaddr*)&cli2, &clientlen))==-1){
+        perror("accept client2");
+        exit(1);
+    }
+
+    printf("client2 connected\n");
+
+    // client2의 username 받음
+    if(recv(cd2,buf,sizeof(buf),0)==-1){
+        perror("recv");
+        exit(1);
+    }
+
+    printf("%s connected\n",buf);
+
+    send(cd1,&cli2,sizeof(cli2),0);
+    send(cd2,&cli1,sizeof(cli1),0);
+
+    printf("Client1 and Client2 is interconnected\n");
+    close(cd1);
+    close(cd2);
     close(sd);
+
+    return 0;
 }
